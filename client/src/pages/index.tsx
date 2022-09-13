fa90d67abefd700df4b1e51e8ebfbbeff6096443
blood8879 <blood8879@gmail.com>
2022-09-13 22:20:00 +0900

    20220913

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -7,7 +7,7 @@ const Home: NextPage = () => {
   return (
     <div className={styles.container}>
       <h1 className='text-3xl font-bold underline'>
-        Hello world!
+        
       </h1>
     </div>
   )
