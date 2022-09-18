5e0f63988ed4d1514c9925d45e68e7901c7bdd2c
blood8879 <blood8879@gmail.com>
2022-09-18 21:36:38 +0900

    20220918

--- client/src/components/NavBar.tsx
+++ client/src/components/NavBar.tsx
@@ -1,6 +1,8 @@
 import axios from "axios";
+import Image from "next/image";
 import Link from "next/link";
 import React from "react";
+import { FaSearch } from "react-icons/fa";
 import { useAuthDispatch, useAuthState } from "../context/auth";
 
 const NavBar: React.FC = () => {
@@ -21,15 +23,20 @@ const NavBar: React.FC = () => {
     return (
         <div className="fixed inset-x-0 top-0 z-10 flex items-center justify-between h-16 px-5 bg-white">
             <span className="text-2xl font-semibold text-gray-400">
-                <Link href="/">Community</Link>
+                <Link href="/">
+                    <a>
+                        Reddit
+                    </a>
+                </Link>
             </span>
 
             <div className="max-w-full px-4">
                 <div className="relative flex items-center bg-gray-100 border rounded hover:border-gray-700 hover:bg-white">
+                    <FaSearch className="ml-2 text-gray-400"/>
                     <input 
                         type="text"
-                        placeholder="Search..."
-                        className="px-3 py-1 bg-transparent rounded focus:outline-none"
+                        placeholder="Search Reddit"
+                        className="px-3 py-1 bg-transparent h-7 rounded focus:outline-none"
                     />
                 </div>
             </div>
@@ -38,19 +45,19 @@ const NavBar: React.FC = () => {
                 {!loading && (
                     authenticated ? (
                         <button 
-                            className="w-20 p-2 mr-2 text-center text-white bg-gray-400 rounded"
+                            className="w-20 px-2 mr-2 text-sm h-7 text-center text-white bg-gray-400 rounded"
                             onClick={handleLogout}
                         >
                             로그아웃
                         </button>
                     ) : (<>
                         <Link href="/login">
-                            <a className="w-20 p-2 mr-2 text-center text-blue-500 border border-blue-500 rounded">
+                            <a className="w-20 px-2 pt-1 mr-2 text-sm h-7 text-center text-blue-500 border border-blue-500 rounded">
                                 로그인
                             </a>
                         </Link>
                         <Link href="/register">
-                            <a className="w-20 p-2 text-center text-white bg-gray-400 rounded">
+                            <a className="w-20 px-2 pt-1 mr-2 text-center text-sm h-7 text-white bg-gray-400 rounded">
                                 회원가입
                             </a>
                         </Link>
